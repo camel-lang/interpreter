f7fa627c2d06057dbc001094cbe8674730e88931
Moxitron <70322560+moxitron@users.noreply.github.com>
2022-10-10 09:36:32 +0330

    ADD: added const literal for TokenType

--- token.go
+++ token.go
@@ -8,3 +8,27 @@ struct Token {
 	Type TokenType 
 	Literal string 
 }
+
+const ( 
+
+	ILLEGAL = "ILLEGAL"
+	EOF = "EOF"
+
+	INT = "INT"
+	INDENT = "INDENT"
+
+	PLUS = "+" 
+	ASSIGN = "="
+	
+	COMMA = "," 
+	SEMICOLON = ";" 
+	
+	LPAREN = "(" 
+	RPAREN = ")" 
+	LBRACE = "{" 
+	RBRACE = "}"
+
+	FUNCTION = "FUNCTION" 
+	LET = "LET" 
+
+)
