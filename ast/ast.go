c34821db41808e19cdf81e0aaa0f09ecbd69f5e9
Moxitron <70322560+moxitron@users.noreply.github.com>
2022-10-10 19:51:17 +0330

    ADD: adding the building blocks of AST tree

---
+++ ast/ast.go
@@ -0,0 +1,57 @@
+package ast 
+
+import ( 
+	"xxx/token"
+) 
+
+
+type Node interface{ 
+	TokenLiteral() string
+} 
+
+type Statement interface{ 
+	Node
+	statementNode()
+}
+
+type Expression interface{ 
+	Node 
+	expressionNode() 
+}
+
+type Program struct {
+	Statements []Statement
+}
+
+func (p *Program) TokenLiteral() string { 
+
+	if len(p.Statements) > 0 { 
+		return p.Statements[0].TokenLiteral() 
+	} else { 
+		return ""
+	}
+}
+
+
+type LetStatement struct { 
+	Token token.Token
+	Name *Identifier 
+	Value Expression 
+} 
+
+func (ls *LetStatement) TokenLiteral() string { 
+	return ls.Token.Literal
+}
+func (ls *LetStatement) statementNode () {} 
+
+type Identifier struct {
+	Token token.Token 
+	Value string 
+}
+
+func (i *Identifier) expressionNode() {} 
+func(i *Identifier) TokenLiteral() string {
+	return i.Token.Literal
+}
+
+
