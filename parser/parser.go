fcb47b02f6ef696baaa7be304da9c5ff9c8a41a1
Moxitron <70322560+moxitron@users.noreply.github.com>
2022-10-12 16:18:26 +0330

    ADD: added parsing for letstatemetns

--- parser/parser.go
+++ parser/parser.go
@@ -1,6 +1,5 @@
 package parser 
 
-
 import ( 
 	"xxx/ast" 
 	"xxx/token" 
@@ -29,6 +28,70 @@ func (p *Parser) nextToken() {
 	p.peekToken = p.lex.NextToken() 
 }
 
-func (p *Parser) ParseProgram *ast.Program { 
-	return nil
+func (p *Parser) ParseProgram() (*ast.Program) { 
+	
+	program := &ast.Program{} 
+	program.Statements = []ast.Statement{} 
+	
+	for p.curToken.Type != token.EOF { 
+
+		stmt := p.parseStatement() 
+		if stmt != nil { 
+			program.Statements = append(program.Statements , stmt)
+		}
+		p.nextToken() 
+	}
+	return program 
+}
+
+
+func (p *Parser) parseStatement() ast.Statement { 
+
+	switch p.curToken.Type { 
+
+		case token.LET :
+			return p.parseLetStatement() 
+		default : 
+			return nil 	
+	}
+}
+
+
+func (p *Parser) parseLetStatement() *ast.LetStatement { 
+
+
+	stmt := &ast.LetStatement{Token: p.curToken}
+	 
+	if !p.expectPeek(token.IDENT) {
+		return nil 
+	}
+
+	stmt.Name = &ast.Identifier{Token: p.curToken , Value: p.curToken.Literal}
+	
+	if !p.expectPeek(token.ASSIGN) { 
+		return nil 
+	}
+
+	for !p.curTokenIs(token.SEMICOLON) { 
+		p.nextToken() 
+	}
+
+	return stmt 
+}	
+
+func (p *Parser) curTokenIs(tok token.TokenType) bool { 
+	return p.curToken.Type == tok 
+} 
+
+func (p *Parser) peekTokenIs(tok token.TokenType) bool { 
+	return p.peekToken.Type == tok 
+}
+
+func (p *Parser) expectPeek(tok token.TokenType) bool { 
+	if p.peekTokenIs(tok) { 
+		p.nextToken()
+		return true 
+	} else { 
+		return false 
+	}
 }
