3d9fcffbfce4f16a4ad190a320fb2d7fcf064bb1
Moxitron <70322560+moxitron@users.noreply.github.com>
2022-10-14 10:42:11 +0330

    ADD: added type IntegerLiteral

--- ast/ast.go
+++ ast/ast.go
@@ -127,3 +127,15 @@ func (exs *ExpressionStatement) String() string {
 } 
 
 
+type IntegerLiteral struct {
+	Token token.Token 
+	Value int64
+}
+
+func (il *IntegerLiteral) expressionNode() {} 
+func (il *IntegerLiteral) TokenLiteral() string { 
+	return il.Token.Literal 
+} 
+func (il *IntegerLiteral) String() string {
+	return il.Token.Literal
+} 
