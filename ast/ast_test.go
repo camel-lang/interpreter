24ebd31ca254c4ac4322e9f9a178243dfcdbb9e4
Moxitron <70322560+moxitron@users.noreply.github.com>
2022-10-14 10:11:15 +0330

    ADD: added test for String method in ast.Program

---
+++ ast/ast_test.go
@@ -0,0 +1,32 @@
+package ast
+
+import ( 
+	"testing" 
+	"camel/token"
+)
+
+
+func TestString(t *testing.T) { 
+	
+	program := &Program{ 
+		Statements: []Statement { 
+			&LetStatement{  
+			Token: token.Token{Type: token.LET ,Literal: "let"}, 
+			Name: &Identifier{ 
+				Token: token.Token{Type: token.IDENT , Literal: "myVar"} , 
+				Value: "myVar" , 
+				} , 
+			Value: &Identifier{ 	
+				Token: token.Token{Type: token.IDENT , Literal: "anotherVar"} , 
+				Value : "anotherVar" , 
+				}, 
+			},
+		},
+	}
+	
+	if program.String() != "let myVar = anotherVar;" { 
+
+		t.Errorf("program.String() is wrong, got %q" , 
+		program.String())
+	}	
+}
