71099a66c04916b6a0cb1196895584c969402ecd
Moxitron <70322560+moxitron@users.noreply.github.com>
2022-10-14 11:49:46 +0330

    ADD: added prefix expression type

--- ast/ast.go
+++ ast/ast.go
@@ -139,3 +139,29 @@ func (il *IntegerLiteral) TokenLiteral() string {
 func (il *IntegerLiteral) String() string {
 	return il.Token.Literal
 } 
+
+type PrefixExpression struct { 
+	Token token.Token 
+	Operator string 
+	Right Expression 
+}
+
+func (pe *PrefixExpression) expressionNode() {} 
+func (pe *PrefixExpression) TokenLiteral() string {
+	return pe.Token.Literal 
+} 
+
+func (pe *PrefixExpression) String() string { 
+	
+	var out bytes.Buffer 
+
+	out.WriteString(pe.Operator) 
+	out.WriteString("(") 
+	out.WriteString(pe.Right.String()) 
+	out.WriteString(")")
+
+	return out.String() 
+}
+
+
+ 
