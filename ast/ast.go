271008ec2fa28a8aca8f1a24d3e26f4e700f9bd3
Moxitron <70322560+moxitron@users.noreply.github.com>
2022-10-16 23:21:12 +0330

    ADD: added IfExpression & BlockStatement in ast.go

--- ast/ast.go
+++ ast/ast.go
@@ -198,3 +198,46 @@ func (b *Boolean) TokenLiteral() string {
 func (b *Boolean) String() string { 
 	return b.Token.Literal 
 }
+
+type IfExpression struct { 
+	Token token.Token 
+	Condition Expression 
+	Consequence *BlockStatement 
+	Alternative *BlockStatement
+}
+
+func (ie *IfExpression) TokenLiteral() string { 
+	return ie.Token.Literal
+}
+func (ie *IfExpression) String() string { 
+	var out bytes.Buffer 
+	
+	out.WriteString("if")
+	out.WriteString(ie.Condition.String())
+	out.WriteString(" ")
+	out.WriteString(ie.Consequence.String()) 
+	
+	if ie.Alternative != nil { 
+		out.WriteString("else") 
+		out.WriteString(ie.Alternative.String()) 
+	} 
+
+	return out.String()
+}
+
+type BlockStatement { 
+	Token token.Token 
+	Statements []Statement 
+} 
+
+func (bs *BlockStatement) statementNode() {} 
+func (bs *BlockStatement) TokenLiteral() string { 
+	return Token.Literal 
+}
+func (bs *BlockStatement) String() string { 
+	var out bytes.Buffer 
+	for _ , s := range bs.Statements { 
+		out.WriteString(s.String()) 
+	} 
+	return out.String() 
+}
