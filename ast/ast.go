3912466ebecc80cda79fc07b5edddc0659354e21
Moxitron <70322560+moxitron@users.noreply.github.com>
2022-10-17 00:09:10 +0330

    ADD: added FunctionLiterals

--- ast/ast.go
+++ ast/ast.go
@@ -242,3 +242,30 @@ func (bs *BlockStatement) String() string {
 	} 
 	return out.String() 
 }
+
+type FunctionLiterals struct { 
+	Token token.Token 
+	Parameters []*Identifier 
+	Body *BlockStatement 
+}
+
+func (fl *FunctionLiterals) expressionNode() {} 
+func (fl *FunctionLiterals) TokenLiteral() string { 
+	return fl.Token.Literal
+}
+
+func(fl *FunctionLiterals) String() string { 
+	var out bytes.Buffer 
+	
+	param := []string{} 
+	for _ , p := range fl.Parameters { 
+		params = append(params , p)
+	}
+	out.WriteString(fl.TokenLiteral()) 
+	out.WriteString("(")
+	out.WriteString(strings.Join(params , ", ") 
+	out.WriteString(")")
+	out.WriteString(fl.Body.String()) 
+
+	return out.String()
+}
