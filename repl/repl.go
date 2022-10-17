c4d67a638d5b735cfec2c0ca680d9f032dd0eea1
Moxitron <70322560+moxitron@users.noreply.github.com>
2022-10-17 10:34:04 +0330

    UPDATE: updated repl to support parsing

--- repl/repl.go
+++ repl/repl.go
@@ -3,7 +3,7 @@ package repl
 import (
 	"bufio"
 	"camel/lexer"
-	"camel/token"
+	"camel/parser"
 	"fmt"
 	"io"
 )
@@ -42,8 +42,8 @@ func Start(in io.Reader, out io.Writer) {
 
 		line := scanner.Text()
 		lex := lexer.New(line)
-		for tok := lex.NextToken(); tok.Type != token.EOF; tok = lex.NextToken() {
-			fmt.Printf("%+v\n", tok)
-		}
+		parser := parser.New(lex) 
+		program := parser.ParseProgram() 
+		fmt.Printf("%s\n" , program.String()) 
 	}
 }
