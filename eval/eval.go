cc2476eaab1ee67f530244c8c1559c1cb3ed470f
Moxitron <70322560+moxitron@users.noreply.github.com>
2022-10-17 23:16:48 +0330

    ADD: added support for parsing  mathematical expressions

--- eval/eval.go
+++ eval/eval.go
@@ -23,6 +23,11 @@ func Eval(node ast.Node) object.Object {
 	case *ast.PrefixExpression : 
 		right := Eval(node.Right) 
 		return evalPrefixExpression(node.Operator, right) 
+	
+	case *ast.InfixExpression : 
+		left := Eval(node.Left) 
+		right := Eval(node.Right) 
+		return evalInfixExpression(node.Operator, left, right) 
 
 	case *ast.IntegerLiteral : 
 		return &object.Integer{Value: node.Value} 
@@ -97,4 +102,44 @@ func evalMinusPrefixOperator(obj object.Object) object.Object {
 
 	val := obj.(*object.Integer).Value
 	return &object.Integer{Value: -val}
+}
+
+func evalInfixExpression(
+	operator string, 
+	left, right object.Object,
+) object.Object { 
+
+
+	switch { 
+
+	case left.Type() == object.INTEGER_OBJ && 
+		 right.Type() == object.INTEGER_OBJ :
+		return parseIntegerInfixExpression(operator, left, right) 
+
+	default : 
+		return NULL
+	}
+} 
+
+func parseIntegerInfixExpression(
+	operator string, 
+	left, right object.Object,
+) object.Object { 
+	
+	leftVal := left.(*object.Integer).Value
+	rightVal := right.(*object.Integer).Value 
+
+	switch operator { 
+
+	case "+": 
+		return &object.Integer{Value: leftVal + rightVal} 
+	case "-":
+		return &object.Integer{Value: leftVal - rightVal} 
+	case "/": 
+		return &object.Integer{Value: leftVal / rightVal} 
+	case "*": 
+		return &object.Integer{Value: leftVal * rightVal} 
+	default : 
+		return NULL
+	}
 }
