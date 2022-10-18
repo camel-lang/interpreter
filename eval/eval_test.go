943d58b51059d1d611f11449f6b8377bdd91539a
Moxitron <70322560+moxitron@users.noreply.github.com>
2022-10-18 23:47:33 +0330

    TEST: added tests for let statement

--- eval/eval_test.go
+++ eval/eval_test.go
@@ -213,6 +213,24 @@ if (10 > 1) {
 		}
 	}
 }
+
+
+func TestLetStatements(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected int64
+	}{
+		{"let a = 5; a;", 5},
+		{"let a = 5 * 5; a;", 25},
+		{"let a = 5; let b = a; b;", 5},
+		{"let a = 5; let b = a; let c = a + b + 5; c;", 15},
+	}
+
+	for _, tt := range tests {
+		testIntegerObject(t, testEval(tt.input), tt.expected)
+	}
+}
+
 func testIntegerObject(t *testing.T, obj object.Object, expected int64) bool {
 
 	result, ok := obj.(*object.Integer)
