b9b2c4794a7723b707cbc285f6345d06108ad888
Moxitron <70322560+moxitron@users.noreply.github.com>
2022-10-20 15:35:39 +0330

    FIX: fixed bug at builtin

--- eval/builtins.go
+++ eval/builtins.go
@@ -2,7 +2,7 @@ package eval
 
 import "camel/object" 
 
-var builtins = map[string]:*object.Builtin{
+var builtins = map[string]*object.Builtin{
 		"len" : &object.Builtin{
 			Fn: func(args ...object.Object) object.Object { 
 				return NULL
