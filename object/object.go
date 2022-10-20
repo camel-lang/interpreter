b666d47adb74073411153599d124c864543e69d0
Moxitron <70322560+moxitron@users.noreply.github.com>
2022-10-20 10:23:57 +0330

    ADD: added Function to objects

--- object/object.go
+++ object/object.go
@@ -2,6 +2,9 @@ package object
 
 import ( 
 	"fmt"
+	"camel/ast" 
+	"strings"
+	"bytes" 
 )
 
 type ObjectType string
@@ -13,6 +16,7 @@ const (
 	NULL        = "NULL"
 	RETURN_VALUE_OBJ = 	"RETURN_VALUE"
 	ERROR_OBJ = "ERROR"
+	FUNCTION_OBJ = "FUNCTION"
 )
 
 type Object interface { 
@@ -74,4 +78,35 @@ func (e *Error) Type() ObjectType {
 } 
 func (e *Error) Inspect() string { 
 	return "Error: " + e.Message
+}
+
+type Function struct { 
+	Parameters []*ast.Identifier
+	Body *ast.BlockStatement
+	Env *Environment 
+}
+
+func (f *Function) Type() ObjectType { 
+	return FUNCTION_OBJ 
+} 
+
+func (f *Function) Inspect() string { 
+	
+	var out bytes.Buffer 
+	
+	params := []string{}
+	for _, p := range f.Parameters { 
+		params = append(params, p.String()) 
+	}
+	
+	out.WriteString("fn") 
+	out.WriteString("(")
+	out.WriteString(strings.Join(params, ", "))
+	out.WriteString(")")
+	out.WriteString(f.Body.String())
+	out.WriteString("{\n")
+	out.WriteString("\n}")
+	
+	return out.String() 
+
 }
