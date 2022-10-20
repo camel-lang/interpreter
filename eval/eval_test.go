848d114f6ccec21f1ba7326ae0c6240c3232b7bd
Moxitron <70322560+moxitron@users.noreply.github.com>
2022-10-20 12:41:29 +0330

    TEST: added tests for string concatenation

--- eval/eval_test.go
+++ eval/eval_test.go
@@ -316,6 +316,19 @@ func TestStringLiteral(t *testing.T) {
 		t.Errorf("String has wrong value. got=%q", str.Value)
 	}
 }
+func TestStringConcatenation(t *testing.T) {
+	input := `"Hello" + " " + "World!"`
+
+	evaluated := testEval(input)
+	str, ok := evaluated.(*object.String)
+	if !ok {
+		t.Fatalf("object is not String. got=%T (%+v)", evaluated, evaluated)
+	}
+
+	if str.Value != "Hello World!" {
+		t.Errorf("String has wrong value. got=%q", str.Value)
+	}
+}
 
 func testIntegerObject(t *testing.T, obj object.Object, expected int64) bool {
 
