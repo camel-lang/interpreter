6359c5227f46c6c0d5d8fc6c9c7e93e0e0068439
Moxitron <70322560+moxitron@users.noreply.github.com>
2022-10-20 10:45:49 +0330

    ADD: added support for enclosed environments

--- object/environment.go
+++ object/environment.go
@@ -1,16 +1,26 @@
 package object
 
+func NewEnclosedEnvironment(out *Environment) *Environment { 
+	env := NewEnvironment() 
+	env.outer = out
+	return env 
+}
+
 func NewEnvironment() *Environment {
 	store := make(map[string]Object)
-	return &Environment{store: store}
+	return &Environment{store: store, out: nil}
 }
 
 type Environment struct {
 	store map[string]Object
+	outer *Environment
 }
 
 func (e *Environment) Get(name string) (Object, bool) {
 	obj, ok := e.store[name]
+	if !ok && e.outer != nil { 
+		obj, ok := e.outer.Get(name)
+	}
 	return obj, ok
 }
 func (e *Environment) Set(name string, val Object) Object {
