a83495f7394652de06d0e425714de9b5c0c362cf
Moxitron <70322560+moxitron@users.noreply.github.com>
2022-10-20 16:44:34 +0330

    ADD: added ArrayLiteral

--- ast/ast.go
+++ ast/ast.go
@@ -310,3 +310,26 @@ func (ce *CallExpression) String() string {
 
 	return out.String()
 }
+
+type ArrayLiteral struct { 
+	Token token.Token
+	Elements []Expression
+} 
+
+func (al *ArrayLiteral) expressionNode() {} 
+func (al *ArrayLiteral) TokenLiteral() string { 
+	return al.Token.Literal 
+}
+func (al *ArrayLiteral) String() string { 
+	var out bytes.Buffer 
+
+	args := []string{} 
+	for _, elem := range al.Elements { 
+		args = append(args, elem.String()) 
+	} 
+	out.WriteString("[") 
+	out.WriteString(strings.Join(args, ", ")) 
+	out.WriteString("]") 
+	
+	return out.String() 
+}
