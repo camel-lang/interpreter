3e5586ff14b3c2992d93bf3d2f98c11f0fa0c39a
Moxitron <70322560+moxitron@users.noreply.github.com>
2022-10-20 17:23:48 +0330

    ADD: added case for arrays in Eval

--- eval/eval.go
+++ eval/eval.go
@@ -45,6 +45,13 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 		}
 
 		return applyFunction(function, args) 
+	
+	case *ast.ArrayLiteral : 
+		elements := evalExpressions(node.Elements, env) 
+		if len(elements) == 1 && isError(elements[0]) { 
+			return elements[0]
+		}		
+		return &object.Array{Elements: elements} 
 
 	case *ast.PrefixExpression:
 		right := Eval(node.Right, env)
