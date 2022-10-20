3d72cd3457d1b5132075d900dee98eb306cf3229
Moxitron <70322560+moxitron@users.noreply.github.com>
2022-10-20 12:32:04 +0330

    TEST: added tests for strings

--- eval/eval_test.go
+++ eval/eval_test.go
@@ -303,6 +303,19 @@ addTwo(2)`
 
 	testIntegerObject(t, testEval(input), 4) 
 } 
+func TestStringLiteral(t *testing.T) {
+	input := `"Hello World!"`
+
+	evaluated := testEval(input)
+	str, ok := evaluated.(*object.String)
+	if !ok {
+		t.Fatalf("object is not String. got=%T (%+v)", evaluated, evaluated)
+	}
+
+	if str.Value != "Hello World!" {
+		t.Errorf("String has wrong value. got=%q", str.Value)
+	}
+}
 
 func testIntegerObject(t *testing.T, obj object.Object, expected int64) bool {
 
