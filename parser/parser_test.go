8fb7effa93b6d53af0e0cb2c5bc7f5265dc5d656
Moxitron <70322560+moxitron@users.noreply.github.com>
2022-10-20 17:13:25 +0330

    TEST: added tests for array literals

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -356,6 +356,27 @@ func TestOperatorPrecedenceParsing(t *testing.T) {
 		}
 	}
 }
+func TestParsingIndexExpressions(t *testing.T) {
+	input := "myArray[1 + 1]"
+
+	l := lexer.New(input)
+	p := New(l)
+	program := p.ParseProgram()
+
+	stmt, ok := program.Statements[0].(*ast.ExpressionStatement)
+	indexExp, ok := stmt.Expression.(*ast.IndexExpression)
+	if !ok {
+		t.Fatalf("exp not *ast.IndexExpression. got=%T", stmt.Expression)
+	}
+
+	if !testIdentifier(t, indexExp.Left, "myArray") {
+		return
+	}
+
+	if !testInfixExpression(t, indexExp.Index, 1, "+", 1) {
+		return
+	}
+}
 
 func TestBooleanExpression(t *testing.T) {
 	tests := []struct {
