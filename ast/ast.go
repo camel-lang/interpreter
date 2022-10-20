98facf966b90616391487985858c057954d77ad8
Moxitron <70322560+moxitron@users.noreply.github.com>
2022-10-20 16:53:56 +0330

    ADD: added IndexExpression in ast

--- ast/ast.go
+++ ast/ast.go
@@ -333,3 +333,26 @@ func (al *ArrayLiteral) String() string {
 	
 	return out.String() 
 }
+
+type IndexExpression struct { 
+	Token token.Token 
+	Left Expression 
+	Index Expression 
+} 
+
+func (ie *IndexExpression) expressionNode() {} 
+func (ie *IndexExpression) TokenLiteral() string { 
+	return ie.Token.Literal
+}
+func (ie *IndexExpression) String() string { 
+	var out bytes.Buffer 
+
+	out.WriteString("(") 
+	out.WriteString(ie.Index.String()) 
+	out.WriteString("[") 
+	out.WriteString(ie.Left.String()) 
+	out.WriteString("]") 
+	out.WriteString(")") 
+	
+	return out.String()
+} 
