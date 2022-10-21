3e404e21460bea0ba1725e2d37b7bf998a47ac4f
Moxitron <70322560+moxitron@users.noreply.github.com>
2022-10-21 11:32:27 +0330

    ADD: added support for hashing

--- object/object.go
+++ object/object.go
@@ -5,6 +5,7 @@ import (
 	"camel/ast"
 	"fmt"
 	"strings"
+	"hash/fnv"
 )
 
 type ObjectType string
@@ -15,6 +16,7 @@ const (
 	STRING_OBJ 	     = "STRING" 
 	BOOLEAN_OBJ      = "BOOLEAN"
 	ARRAY_OBJ 		 = "ARRAY"
+	HASH_OBJ 		 = "HASH" 
 	NULL             = "NULL"
 	RETURN_VALUE_OBJ = "RETURN_VALUE"
 	ERROR_OBJ        = "ERROR"
@@ -157,4 +159,68 @@ func (a *Array) Inspect() string {
 	out.WriteString("]") 
 	
 	return out.String() 
+}
+
+type Hashable interface { 
+	HashKey() HashKey 
 } 
+
+type HashKey struct { 
+	Type ObjectType
+	Value uint64
+}   
+
+
+func (b *Boolean) HashKey() HashKey { 
+	
+	var value uint64 
+		
+	if b.Value { 
+		value = 1 
+	} else { 
+		value = 0 
+	} 
+
+	return HashKey{Type: b.Type(), Value: value} 
+} 
+func (i *Integer) HashKey() HashKey { 
+	return HashKey{Type: i.Type(), Value: uint64(i.Value)}
+} 
+func (s *String) HashKey() HashKey { 
+	
+	h := fnv.New64a() 
+	h.Write([]byte(s.Value)) 
+
+	return HashKey{Type: s.Type(), Value: h.Sum64()}
+}
+
+type HashPair struct { 
+	Key Object 
+	Value Object 
+} 
+
+type Hash struct { 
+	Pairs map[HashKey]HashPair
+}
+
+func (h *Hash) Type() ObjectType { 
+	return HASH_OBJ
+} 
+
+func (h *Hash) Inspect() string { 
+	
+	var out bytes.Buffer 
+	
+	pairs := []string{}
+
+	for _, p := range h.Pairs { 
+		pairs = append(pairs, fmt.Sprintf("%s : %s",
+		p.Key.Inspect(), p.Value.Inspect()))
+	} 
+
+	out.WriteString("{") 
+	out.WriteString(strings.Join(pairs, ", ")) 
+	out.WriteString("}") 
+	
+	return out.String() 
+}  
