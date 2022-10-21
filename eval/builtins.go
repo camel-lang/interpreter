c20fc29cd9adde8fa804eb40c82e09f25214febc
Moxitron <70322560+moxitron@users.noreply.github.com>
2022-10-21 09:37:51 +0330

    ADD: added array case to builtin

--- eval/builtins.go
+++ eval/builtins.go
@@ -16,6 +16,8 @@ var builtins = map[string]*object.Builtin{
 				
 				case *object.String : 
 					return &object.Integer{Value: int64(len(arg.Value))} 
+				case *object.Array : 
+					return &object.Integer{Value: int64(len(arg.Elements))} 
 				default : 
 					return newError( 
 				"argument to `len` not supported, got: " + 
